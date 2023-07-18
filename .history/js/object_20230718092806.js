ec8ecf65462822b8715dada558b7e3dc8e295c8a
nguyentrongbut <115614825+nguyentrongbut@users.noreply.github.com>
2023-07-18 14:16:15 +0700

    object

---
+++ .history/js/object_20230718092806.js
@@ -0,0 +1,40 @@
+// Cách khai báo object
+// object literal
+const objectLiteral = {};
+// object constructor
+const objectConstructor = new Object();
+const student = {
+    name: "Cloly",
+    age: 21,
+    male: true,
+    "last-name": "but",
+    hi: function() {
+        console.log("Hello Cloly");
+    },
+};
+// properties
+// method
+// 2 cách truy xuất giá trị của object 
+// 1. Dot notation .
+console.log(student.name);
+// 2. Bracket notation ["key"]
+console.log(student["age"]);
+console.log(student["last-name"]);
+// Thay đổi giá trị của object
+student.age = 20;
+student.male = "male";
+// Thêm 
+student.isDeveloper = true;
+// is-developer
+student["is-developer"] = false;
+student.hello = function() {
+    console.log("hello");
+}
+console.log(student);
+// Cách xóa giá trị trong Object
+delete student["last-name"];
+// for in hay dùng cho object for of hay dùng cho mảng
+for (let key in student) {
+    if(key )
+    console.log(`${key}:${student[key]}`);
+}
---
+++ .history/js/object_20230718120515.js
@@ -0,0 +1,61 @@
+// Cách khai báo object
+// object literal
+const objectLiteral = {};
+// object constructor
+const objectConstructor = new Object();
+const student = {
+    name: "Cloly",
+    age: 21,
+    male: true,
+    "last-name": "but",
+    hi: function() {
+        console.log("Hello Cloly");
+    },
+};
+// properties
+// method
+// 2 cách truy xuất giá trị của object 
+// 1. Dot notation .
+console.log(student.name);
+// 2. Bracket notation ["key"]
+console.log(student["age"]);
+console.log(student["last-name"]);
+// Thay đổi giá trị của object
+student.age = 20;
+student.male = "male";
+// Thêm 
+student.isDeveloper = true;
+// is-developer
+student["is-developer"] = false;
+student.hello = function() {
+    console.log("hello");
+}
+console.log(student);
+// Cách xóa giá trị trong Object
+delete student["last-name"];
+// for in hay dùng cho object for of hay dùng cho mảng
+for (let key in student) {
+    if(key === "name") {
+        console.log("hello name");
+    }
+    const value = student[key];
+    console.log(`${key}:${value}`);
+}
+
+// Object.keys(object) -> trả về 1 mảng chứa tất cả các keys của object
+const keys = Object.keys(student);
+console.log(keys);
+console.log(keys.length);
+// Object.values(object) -> trả về 1 mảng chứa tất cả các value của object
+const values = Object.values(student);
+console.log(values);
+// Object.entries(object) -> trả về 1 mảng nested gồm có key và value
+const entries = Object.entries(student);
+console.log(entries);
+// Object.assign()
+const a = {
+    firstName : "But",
+};
+const b = {
+    lastName: "Trong",
+}
