012d9441c3df1e08395c835e27abcb3db7094721
nguyentrongbut <115614825+nguyentrongbut@users.noreply.github.com>
2023-10-16 15:37:03 +0700

    memory-game done

--- exercises/memory-game.js
+++ exercises/memory-game.js
@@ -39,17 +39,31 @@ let secondGuess = "";
 const delay = 1000;
 const grid = document.querySelector(".grid");
 // array.sort(() => 0.5 - Math.random()); random array
-const cardsArrayMerge = cardsArray
-    .concat(cardsArray)
-    .sort(() => 0.5 - Math.random());
-cardsArrayMerge.forEach((item) => {
-    const card = document.createElement("div");
-    card.classList.add("card");
-    card.setAttribute("data-name", item.name);
-    // card.dataset.name = item.name;
-    card.style.backgroundImage = `url(${item.img})`;
-    grid.appendChild(card);
-});
+function generateCard() {
+    // reset innerHTML
+    grid.innerHTML = "";
+    const cardsArrayMerge = cardsArray
+        .concat(cardsArray)
+        .sort(() => 0.5 - Math.random());
+    cardsArrayMerge.forEach((item) => {
+        const card = document.createElement("div");
+        card.classList.add("card");
+        card.setAttribute("data-name", item.name);
+        // front card
+        const front = document.createElement("div");
+        front.classList.add("front");
+        // back card
+        const back = document.createElement("div");
+        back.classList.add("back");
+        // back card
+        // card.dataset.name = item.name;
+        back.style.backgroundImage = `url(${item.img})`;
+        card.appendChild(front);
+        card.appendChild(back);
+        grid.appendChild(card);
+    });
+}
+generateCard();
 function matchingCard() {
     const selects = document.querySelectorAll(".selected");
     [...selects].forEach((item) => item.classList.add("matched"));
@@ -58,33 +72,48 @@ function resetGuess() {
     count = 0;
     firstGuess = "";
     secondGuess = "";
+    previousCard = null;
     const selects = document.querySelectorAll(".selected");
+    const matchedAll = document.querySelectorAll(".matched");
+    const cardLength = document.querySelectorAll(".card").length;
     [...selects].forEach((item) => item.classList.remove("selected"));
+    if (matchedAll.length === cardLength) {
+        // done game -> reset game
+        setTimeout(
+            matchedAll.forEach((el) => el.classList.remove("matched")),
+            1000
+        );
+        setTimeout(generateCard, delay);
+    }
 }
 grid.addEventListener("click", function (e) {
     const clicked = e.target;
-    if (clicked.nodeName === "SECTION" || previousCard === clicked) {
+    if (
+        clicked.nodeName === "SECTION" ||
+        previousCard === clicked ||
+        clicked.parentNode.classList.contains("selected") ||
+        clicked.parentNode.classList.contains("matched")
+    ) {
         return;
     }
     if (count < 2) {
         count++;
         if (count === 1) {
-            firstGuess = clicked.dataset.name;
-            clicked.classList.add("selected");
+            firstGuess = clicked.parentNode.dataset.name;
+            clicked.parentNode.classList.add("selected");
         } else {
-            secondGuess = clicked.dataset.name;
-            clicked.classList.add("selected");
+            secondGuess = clicked.parentNode.dataset.name;
+            clicked.parentNode.classList.add("selected");
         }
 
-        if (firstGuess !== "" && secondGuess !== "") {
+        // if (firstGuess !== "" && secondGuess !== "")
+        if (firstGuess && secondGuess) {
             if (firstGuess === secondGuess) {
                 // handle match here
                 setTimeout(matchingCard, delay);
-                setTimeout(resetGuess, delay);
-            } else {
-                setTimeout(resetGuess, delay);
             }
+            setTimeout(resetGuess, delay);
         }
+        previousCard = clicked;
     }
-    previousCard = clicked;
 });
